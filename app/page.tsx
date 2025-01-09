b8fe3dc25424f8cb61ef9da501cf93bd7ab8012d
kyyril <khairilrahman55@gmail.com>
2025-01-09 10:09:56 +0700

    setup db

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1>halo dunia</h1>
+    <div className="max-w-screen-xl mx-auto py-6 p-4">
+      <h1 className="text-2xl">halo dunia</h1>
     </div>
   );
 }
